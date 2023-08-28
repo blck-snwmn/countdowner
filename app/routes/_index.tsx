261b9a89afc9f935120f3ba5b43995c817788e86
blck-snwmn <whitesnowmancreator@gmail.com>
2023-08-29 01:35:16 +0900

    Add local

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -68,7 +68,7 @@ export default function Index() {
         {/* {r.toLocaleDateString()} */}
       </div>
       <div>
-        {limit.toLocaleDateString()}
+        {limit.toLocaleDateString("ja-JP")}
       </div>
     </div>
   );
